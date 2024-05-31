9001158e2239c93cb933325fa12afe6d603cf007
ian1105102 <ian105102@gmail.com>
2024-06-01 02:25:05 +0800

    feat: Update stock data retrieval to fetch from last update date
    This commit modifies the stock data retrieval process to fetch data from the last update date instead of a fixed date. This change improves the accuracy of the stock data and ensures that the system always displays the most recent information.

---
+++ Stock/static/js/index.js
@@ -0,0 +1,70 @@
+console.log('index.js loaded')
+let input = document.querySelector('.input_contnet input');
+let Search_box = document.querySelector('.Search_box');
+let button = document.querySelector('.submit_btn');
+let search = document.querySelector('.search');
+let Down_menu = document.querySelector('.Down_menu');
+
+button.addEventListener('click', function() {
+    let inputValue = input.value;
+    let queue = getQueueFromCookie();
+
+    if (queue.includes(inputValue)) {
+        return; 
+    }
+
+    if (queue.length >= 4) {
+        queue.shift(); 
+    }
+
+    // 將新值添加到佇列
+    queue.push(inputValue);
+
+    // 更新 Cookie 中的佇列
+    document.cookie = `SearchList=${JSON.stringify(queue)}`;
+});
+
+function getQueueFromCookie() {
+    let queue = [];
+    let cookie = document.cookie;
+
+    if (cookie.includes('SearchList=')) {
+        let startIndex = cookie.indexOf('SearchList=') + 11;    // 11 是 'SearchList=' 的長度
+        let endIndex = cookie.indexOf(';', startIndex);       // 結束索引是分號的索引
+        if (endIndex === -1) {
+            endIndex = cookie.length;                       // 如果找不到分號，則結束索引是 cookie 的長度
+        }
+        let queueString = cookie.slice(startIndex, endIndex);   // 從 cookie 中提取佇列的 JSON 字符串
+        queue = JSON.parse(queueString);                 // 將 JSON 字符串轉換為陣列
+    }       
+
+    return queue;
+}
+function addQueueToHTML() {
+    let queue = getQueueFromCookie();
+    queue = queue.reverse(); // 反轉佇列，以便最新的值顯示在最上面
+    // 清空 Down_menu 中的內容
+    Down_menu.innerHTML = '';
+
+    // 將佇列中的每個值添加到 Down_menu 中
+    queue.forEach(function(value) {
+        Down_menu.innerHTML +=`<p class="Search_item ">${value}</p>`;
+        let Search_item = document.querySelector('.Search_item');
+    });
+    document.querySelectorAll('.Search_item').forEach(function(Search_item) {
+        Search_item.addEventListener('click', function(event) {
+            input.value = event.target.textContent;
+        });
+    });
+}
+
+
+input.addEventListener('focus', function(event) {
+    Search_box.classList.add('active');
+});
+
+input.addEventListener('blur', function(event) {
+    Search_box.classList.remove('active');
+});
+
+addQueueToHTML();
